f0f10070b4a514f4b31a35031b95bd7adcfe2f1c
robinanderson <robin.nicole.anderson@gmail.com>
2015-03-09 16:19:15 -0500

    Delete index.php

--- index.php
+++
@@ -1,9 +0,0 @@
-<html>
-<header>
-<title>Robin Anderson | MS Information Studies Candidate, University of Texas at Austin</title>
-</header>
-<body>
-<h1>Robin Anderson</h1>
-<h2>Coming Soon</h2>
-</body>
-</html>
