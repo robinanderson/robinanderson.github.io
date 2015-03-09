a2b9875eba5db3fa18bcd49f6efbe67dbbdc1e67
robinanderson <robin.nicole.anderson@gmail.com>
2015-03-09 16:11:19 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,9 @@
+<html>
+<header>
+<title>Robin Anderson | MS Information Studies Candidate, University of Texas at Austin</title>
+</header>
+<body>
+<h1>Robin Anderson</h1>
+<h2>Coming Soon</h2>
+</body>
+</html>
